0df48c44c22e2767dd1bb057c0ef81a2a54af68c
Andrey Osin <andreyo@local.pushok.com>
2015-02-26 05:30:52 -0300

    fix

--- modules/userapi.js
+++ modules/userapi.js
@@ -1,5 +1,7 @@
 var path = require('path');
 var cf = require(path.join(__dirname, '..', 'lib', 'config.js'));
+var ApiError = require('./ApiError');
+var _ = require('lodash');
 
 function UserApi(ctx) {
     this._ctx = ctx;
@@ -13,6 +15,25 @@ UserApi.prototype.init = function (cb) {
     self._config = cf();
     cb();
 };
+
+UserApi.prototype.register = function(token, user, cb){
+    var self = this;
+    var session = self.getSession(token);
+    if (!session || !session.user)
+        return cb(new ApiError('Wrong access token', ApiError.Subject.INVALID_TOKEN));
+    if (!user || !user.login ||!_.isString(user.login) || !user.password || !_.isString(user.password) || !user.restId || !user.name || !_.isString(user.name) || !user.role)
+        return cb(new ApiError('Незаполнены обязательны поля', ApiError.Subject.INVALID_DATA));
+    if (user.password.length < 5)
+        return cb(new ApiError('Разрешены пароли > 5 символов', ApiError.Subject.INVALID_DATA));
+    if (!session.user.role.alowRegister || !session.user.allRoles[user.role] || !_.includes(session.user.regRoles, session.user.allRoles[user.role]))
+        return cb(new ApiError('Недостаточно прав', ApiError.Subject.ACCESS_DENIED));
+    var toAdd = {
+        login: user.login,
+        password: user.password
+    };
+
+
+}
 
 module.exports.init = function (ctx, cb) {
     var api = new UserApi(ctx);
